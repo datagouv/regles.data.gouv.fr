d041c1e0ab416ea7062a0abf9a8efe256de7fee6
pouvoirdasha <dariia.haryfullina@gmail.com>
2026-07-28 10:29:10 +0200

    [Fail] Trace explorer - beginning

--- tools/catala/test_retrieval.py
+++ tools/catala/test_retrieval.py
@@ -1,7 +1,31 @@
 import json
+def trace_explorer(trace : dict) -> dict : 
+    """ 
+    Une fonction qui explore l'arbre de la trace 
+    et extrait les valeurs et les noms des inputs avec le tag "only_input"
+    """
+    # parcourt les élements de la trace - à chaque niveau. 
+    ele = trace["element"]
+    
+    if ele["input"] == "only_input" :
+        pos = trace["pos"] 
+        input = pos["value"]
+    
+
+
+
+    # Si input - recupere et continue l'exploration
+
+    # si jamais l'objet trace - si jamais on voit trace n+1 - explorer de trace n+1, sinon sort
+
+
 
 def input_retrieval(trace: dict) -> dict:
     trace = trace[0]["trace"]
+    # Methode 1 - reccursive soit boucle - dès que detecte only input - profondeur puis lecture du champ value - différents niveaux d'imbrication
+
+
+    # Methode 2 regex qui detecte les only_input --> lit la variable plus bas. 
     pass
 
 def main():
